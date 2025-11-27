31c1e89a2ffe19544714ac88d5ca41cc408dad51
Helmi-Y <helmi.yatim7@gmail.com>
2025-11-27 11:49:38 -0700

    Updated formula based on slides

--- compute_metrics.m
+++ compute_metrics.m
@@ -7,8 +7,13 @@ A = double(A);
 B = double(B);
 [m,n] = size(A);
 
-  % ---------- MSE ----------
-     % MSE = 1 / (m * n) * (sum_diff A(i,j) - B(i,j)^2)
+% ---------- MSE ----------
+    % MSE = (1 / N) * Σ (A(i,j) - B(i,j))^2
+    % where:
+    %     A(i,j) = pixel value from the original image
+    %     B(i,j) = pixel value from the processed/noisy image
+    %     N      = total number of pixels = (m * n)
+    
     sum_diff = 0;
     for i = 1:m
         for j = 1:n
@@ -18,23 +23,31 @@ B = double(B);
     end
     MSE = sum_diff / (m*n);
 
-      % ---------- SNR ----------
-     % SNR = signal_sum (A(i,j))^2 / noise_sum  A(i,j) - B(i,j)^2)
-    signal_sum = 0;
-    noise_sum  = 0;
+% ---------- SNR ----------
+    % SNR = 10 * log10( sigma_x^2 / sigma_d^2 )
+    % where:
+    %   sigma_x^2 = average square value of the original image A
+    %   sigma_d^2 = MSE = average squared difference between A and B
 
+    signal_sum = 0;
     for i = 1:m
         for j = 1:n
             signal_sum = signal_sum + A(i,j)^2;
-            noise_sum  = noise_sum + (A(i,j) - B(i,j))^2;
         end
     end
 
-    SNR = signal_sum / noise_sum;
+    sigma_x2 = signal_sum / (m*n);   % average square of original
+    sigma_d2 = MSE;                  % noise variance (MSE)
 
-     % ---------- PSNR ----------
-     % PSNR = 10 * log10 (255^2 / MSE)
+    SNR = 10 * log10(sigma_x2 / sigma_d2);
+
+% ---------- PSNR ----------
+    % PSNR = 10 * log10( X_peak^2 / MSE )
+    % where:
+    %     X_peak = maximum possible pixel value of the original image.
+    %     MSE    = mean squared error between original and noisy images.
     
-    PSNR = 10 * log10( (255^2) / MSE );
+    Xpeak = max(A(:));    % find maximum pixel of original image
+    PSNR = 10 * log10( (Xpeak^2) / MSE );
 
 end
